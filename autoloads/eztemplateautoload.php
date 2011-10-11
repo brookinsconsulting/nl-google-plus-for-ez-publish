e103cfe1594d570478568a01e691437ef6e91870
Nicolas Lescure <fiber.optic0@gmail.com>
2011-10-12 00:32:34 +0200

    template operator alias

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -3,6 +3,6 @@
 $eZTemplateOperatorArray = array();
 $eZTemplateOperatorArray[] = array( 'script' => 'extension/nl_googleplus/autoloads/nl_googleplus.php',
                                     'class' => 'NlGooglePlus',
-                                    'operator_names' => array( 'nlgoogleplus' ) );
+                                    'operator_names' => array( 'nlgoogleplus', 'nlgoogleplusfeed' ) );
 
 ?>
--- autoloads/nl_googleplus.php
+++ autoloads/nl_googleplus.php
@@ -6,7 +6,7 @@ class NlGooglePlus
 
     public function operatorList()
     {
-        return array( 'nlgoogleplus' );
+        return array( 'nlgoogleplus', 'nlgoogleplusfeed' );
     }
 
     public function namedParameterPerOperator()
@@ -16,53 +16,70 @@ class NlGooglePlus
 
     public function namedParameterList()
     {
-        return array( 'nlgoogleplus' => array( 'userid' => array( 'type' => 'string',
-                        'required' => true ) ) );
+        return array(	'nlgoogleplus' => array( 'userid' => array( 'type' => 'string', 'required' => true ) ),
+       					'nlgoogleplusfeed' => array( 'userid' => array( 'type' => 'string', 'required' => true ) )
+        );
     }
 
     public function modify( $tpl, $operatorName, $operatorParameters, &$rootNamespace, &$currentNamespace, &$operatorValue, &$namedParameters )
     {
         switch ( $operatorName )
         {
+        	//G+ feed
+        	//keep "nlgoogleplus" for compatibility
             case 'nlgoogleplus':
+            case 'nlgoogleplusfeed':
             {
-            	//get config
-            	$nlGooglePlusIni = eZINI::instance('nlgoogleplus.ini');
-            	
-            	//initialize Google+ API
-            	$client = new apiClient();
-            	$client->setApplicationName("NL Google Plus for eZ Publish");
-            	// Visit https://code.google.com/apis/console to generate your
-            	// oauth2_client_id, oauth2_client_secret, and to register your oauth2_redirect_uri.
-            	$client->setClientId($nlGooglePlusIni->variable('GooglePlus', 'ClientId'));
-            	$client->setClientSecret($nlGooglePlusIni->variable('GooglePlus', 'ClientSecret'));
-            	$client->setDeveloperKey($nlGooglePlusIni->variable('GooglePlus', 'DeveloperKey'));
-            	$client->setScopes(array($nlGooglePlusIni->variable('GooglePlus', 'Scope')));
-            	//$client->setUseObjects(true);
-            	$plus = new apiPlusService($client);
-            	
-            	if( isset($namedParameters['userid']) ) {
-            		$userId = $namedParameters['userid'];
-            	}
-            	else {
-            		$userId = $nlGooglePlusIni->variable('Feed', 'UserId');
-            	}
-            	
-            	$optParams = array('maxResults' => $nlGooglePlusIni->variable('Feed', 'MaxResults'));
-            	try {
-            		$activities = $plus->activities->listActivities($userId, 'public', $optParams);
-            	}
-            	catch(Exception $e) {
-            		eZLog::write('Google Plus get activities problem : '.$e->getMessage(),'error.log');
-            	}
-            	$items = $activities['items'];
-            	
-            	//use template
-            	$tpl->setVariable('activities',$activities);
-            	$tpl->setVariable('items',$items);
-                $operatorValue = $tpl->fetch( 'design:nlgoogleplus/googleplusbox.tpl' );
+                $operatorValue = $this->getGooglePlusFeed($tpl,$namedParameters);
             } break;
         }
+    }
+    
+    private function getGooglePlusFeed($tpl,$namedParameters) {
+    	//get config
+    	$nlGooglePlusIni = eZINI::instance('nlgoogleplus.ini');
+    	 
+    	//initilize G+ Service
+    	$plus = $this->initializeGooglePlusService();
+    	 
+    	if( isset($namedParameters['userid']) ) {
+    		$userId = $namedParameters['userid'];
+    	}
+    	else {
+    		$userId = $nlGooglePlusIni->variable('Feed', 'UserId');
+    	}
+    	 
+    	$optParams = array('maxResults' => $nlGooglePlusIni->variable('Feed', 'MaxResults'));
+    	try {
+    		$activities = $plus->activities->listActivities($userId, 'public', $optParams);
+    	}
+    	catch(Exception $e) {
+    		eZLog::write('Google Plus get activities problem : '.$e->getMessage(),'error.log');
+    	}
+    	$items = $activities['items'];
+    	
+    	//use template
+    	$tpl->setVariable('activities',$activities);
+    	$tpl->setVariable('items',$items);
+    	return $tpl->fetch( 'design:nlgoogleplus/googleplusbox.tpl' );
+    	
+    }
+    
+    private function initializeGooglePlusService() {
+    	//get config
+    	$nlGooglePlusIni = eZINI::instance('nlgoogleplus.ini');
+    	
+    	//initialize Google+ API
+    	$client = new apiClient();
+    	$client->setApplicationName("NL Google Plus for eZ Publish");
+    	// Visit https://code.google.com/apis/console to generate your
+    	// oauth2_client_id, oauth2_client_secret, and to register your oauth2_redirect_uri.
+    	$client->setClientId($nlGooglePlusIni->variable('GooglePlus', 'ClientId'));
+    	$client->setClientSecret($nlGooglePlusIni->variable('GooglePlus', 'ClientSecret'));
+    	$client->setDeveloperKey($nlGooglePlusIni->variable('GooglePlus', 'DeveloperKey'));
+    	$client->setScopes(array($nlGooglePlusIni->variable('GooglePlus', 'Scope')));
+    	//$client->setUseObjects(true);
+    	return new apiPlusService($client);
     }
 }
 
